6f1527b4c72a6e582ba1b20e5df680e32d8cfe65
thejandulakara <thejandulakaraz10@gmail.com>
2025-12-16 20:30:23 +0530

    Update StackQueue_EnqueueFriendly.java

--- StackQueue_EnqueueFriendly.java
+++ StackQueue_EnqueueFriendly.java
@@ -5,12 +5,11 @@ public class StackQueue_EnqueueFriendly {
     Stack<Integer> s1 = new Stack<>();
     Stack<Integer> s2 = new Stack<>();
 
-    // Enqueue operation (O(1))
     public void enqueue(int x) {
         s1.push(x);
     }
 
-    // Dequeue operation (O(n))
+  
     public int dequeue() {
         if (s1.isEmpty() && s2.isEmpty()) {
             throw new RuntimeException("Queue is empty");
