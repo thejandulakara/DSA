80ce5086adb4db320add884ae180d011d56199d5
thejandulakara <thejandulakaraz10@gmail.com>
2025-12-30 21:43:11 +0530

    Update ArrayCircularQueue.java

---
+++ ArrayCircularQueue.java
@@ -0,0 +1,64 @@
+package dsa.queue;
+
+public class ArrayCircularQueue {
+
+    int[] queue;
+    int front, rear, size, capacity;
+
+    public ArrayCircularQueue(int capacity) {
+        this.capacity = capacity;
+        queue = new int[capacity];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    // ENQUEUE operation
+    public void enqueue(int x) {
+        if (size == capacity) {
+            System.out.println("Queue is full");
+            return;
+        }
+
+        rear = (rear + 1) % capacity;
+        queue[rear] = x;
+        size++;
+        System.out.println("Enqueued: " + x);
+    }
+
+    // DEQUEUE operation
+    public int dequeue() {
+        if (size == 0) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        int removed = queue[front];
+        front = (front + 1) % capacity;
+        size--;
+        System.out.println("Dequeued: " + removed);
+        return removed;
+    }
+
+    // DISPLAY queue
+    public void display() {
+        System.out.print("Queue elements: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(queue[(front + i) % capacity] + " ");
+        }
+        System.out.println();
+    }
+
+    // MAIN method
+    public static void main(String[] args) {
+
+        ArrayCircularQueue queue = new ArrayCircularQueue(5);
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.dequeue();
+        queue.enqueue(4);
+        queue.display();
+    }
+}
