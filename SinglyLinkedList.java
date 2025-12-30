845a530c772713b071e8f46eb37d761d9c9eb9fb
Teragon <thejandulakaraz10@gmail.com>
2025-12-30 21:48:00 +0530

    Refactor comments in SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -14,7 +14,7 @@ public class SinglyLinkedList {
 
     Node head = null;
 
-    // INSERT at end
+
     public void insert(int data) {
         Node newNode = new Node(data);
 
@@ -31,7 +31,7 @@ public class SinglyLinkedList {
         System.out.println("Inserted: " + data);
     }
 
-    // DISPLAY list
+
     public void display() {
         Node temp = head;
         System.out.print("Linked List: ");
@@ -42,7 +42,7 @@ public class SinglyLinkedList {
         System.out.println("null");
     }
 
-    // MAIN method
+
     public static void main(String[] args) {
 
         SinglyLinkedList list = new SinglyLinkedList();
